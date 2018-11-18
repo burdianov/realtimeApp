fe0578699b3339e52a2110af73b18069833227ab
Lilian Burdianov <lilian.burdianov@gmail.com>
2018-11-18 21:51:30 +0400

    Build the relationships

--- app/User.php
+++ app/User.php
@@ -2,9 +2,9 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Question;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -27,4 +27,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function question()
+    {
+        return $this->hasMany(Question::class);
+    }
 }
